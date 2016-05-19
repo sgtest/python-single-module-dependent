d1fc1b55e245ae328f551fb25f76f0108a903ac4
Beyang Liu <beyang@sourcegraph.com>
2016-05-18 23:16:16 -0700

    specify module in py_modules

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup
 setup(
     name='single-module-dependent',
     version='0.0.0',
+    py_modules=['app'],
     url='http://github.com/sgtest/python-single-module-dependent',
 )
